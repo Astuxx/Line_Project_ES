01d326bdc8e91383da2ca36229cf72e7ebb2ce86
CatalinaOS <riccardo@MBPdiCatalinaOS.home>
2019-11-24 23:52:37 +0100

    blank lines problem solved

--- Prova.java
+++ Prova.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.regex.Pattern;
 import java.util.*;
 import java.io.File;
 import java.io.IOException;
@@ -8,12 +9,14 @@ public class Prova {
 
     public static void main(String Args[]) {
         //System.out.println("Ciao");
-        String regexStr = " ";
+        String regexBlankLine = "^[ \\t]$+"; 
+        String regexMultiSpace = "^[ \\t]+";
 
         // The name of file or path
         //String fileName = "nato_documents/txts/AAMedP-1 EDA V1 E.txt";
         String fileName = "Prova.txt";
         String line = null; //line read
+        String stringacomplete = null;
 
         //create array list to save all string readed
         ArrayList<String> arrlist = new ArrayList<String>();
@@ -26,12 +29,8 @@ public class Prova {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             //int i = 0;
             while((line = bufferedReader.readLine()) != null) {
-                //System.out.println(line);
+                stringacomplete=stringacomplete+line;
                 arrlist.add(line);
-                /*i++;
-                if (i==10) {
-                    break;
-                }*/
             }
 
             // Close file
@@ -46,41 +45,14 @@ public class Prova {
             System.out.println(
                 "Error reading file '" + fileName + "'");
         }
-        //end
+        
+        Pattern p = Pattern.compile(regexBlankLine, Pattern.MULTILINE);
+        System.out.println("FIRST REMOVE \n");
+        System.out.println(p.matcher(stringacomplete).replaceAll("")); //ok works
+        
+        p = Pattern.compile(regexMultiSpace);
+        System.out.println("SECOND REMOVE \n");
+        System.out.println(p.matcher(stringacomplete).replaceAll("")); //still eat \n
 
-        String pattern = "\\s+";
-        int i = 0;
-        for (String str : arrlist) {
-            arrlist.set(arrlist.indexOf(str), str.replaceAll("\\s+"," "));
-
-            //System.out.println(str);
-            if(str.isEmpty() ){
-                //arrlist.set(arrlist.indexOf(str), str.replaceAll("([\\n\\r]+\\s*)*$",""));
-                arrlist.remove(str);
-                //System.out.println(i);
-            }
-            //System.out.println(str);
-            i = i+1;
-        }
-
-
-        int pos = 0;
-        int lunghezza = arrlist.size();
-        System.out.println(lunghezza);
-        for ( i =0; i<lunghezza; i++) {
-            System.out.println(arrlist.get(i).substring(arrlist.get(i).length()-1));
-
-            if(!arrlist.get(i).substring(arrlist.get(i).length()-1).matches("\\.") && arrlist.get(i+1).substring(0,1).matches("[a-z_0-9]") && i<(lunghezza) ){
-                arrlist.set(pos, arrlist.get(i)+" "+arrlist.get(i+1));
-                arrlist.remove(arrlist.indexOf(pos+1));
-            }
-        }
-
-        System.out.println("***********");
-        //System.out.println("\n");
-
-        for (String str : arrlist) {
-            System.out.println(str);
-        }
     }
 }
